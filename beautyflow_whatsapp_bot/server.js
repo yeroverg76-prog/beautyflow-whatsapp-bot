29c50fcc015d3b06ea9db6e6f786b533d070f29d
yeroverg76-prog <yeroverg76@gmail.com>
2026-05-08 16:19:24 +0200

    Update server.js

--- beautyflow_whatsapp_bot/server.js
+++ beautyflow_whatsapp_bot/server.js
@@ -516,5 +516,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(\`Siroco bot running on port \${PORT}\`);
+  console.log(`Siroco bot running on port ${PORT}`);
 });
